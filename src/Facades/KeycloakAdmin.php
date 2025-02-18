f8c047e1fb89860b6af6d629f786272286436828
overtrue <anzhengchao@gmail.com>
2025-02-18 14:54:47 +0800

    feat: methods

--- src/Facades/KeycloakAdmin.php
+++ src/Facades/KeycloakAdmin.php
@@ -3,25 +3,27 @@
 namespace Overtrue\LaravelKeycloakAdmin\Facades;
 
 use Fschmtt\Keycloak\Keycloak;
+use Fschmtt\Keycloak\Resource\AttackDetection;
+use Fschmtt\Keycloak\Resource\Clients;
+use Fschmtt\Keycloak\Resource\Groups;
+use Fschmtt\Keycloak\Resource\Organizations;
+use Fschmtt\Keycloak\Resource\Realms;
+use Fschmtt\Keycloak\Resource\Roles;
+use Fschmtt\Keycloak\Resource\ServerInfo;
+use Fschmtt\Keycloak\Resource\Users;
 use Illuminate\Support\Facades\Facade;
 use Overtrue\KeycloakAdmin\Client;
-use Overtrue\KeycloakAdmin\Resources\ClientResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\ClientsResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\RealmResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\RealmsResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\RolesResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\UserResourceInterface;
-use Overtrue\KeycloakAdmin\Resources\UsersResourceInterface;
 
 /**
- * @method static UsersResourceInterface users()
- * @method static UserResourceInterface user(string $id)
- * @method static RolesResourceInterface roles()
- * @method static ClientsResourceInterface clients()
- * @method static Client connection(?string $client)
- * @method static RealmResourceInterface realm(?string $realm = null)
- * @method static ClientResourceInterface client(?string $id = null)
- * @method static RealmsResourceInterface realms()
+ * @method static Users users()
+ * @method static Roles roles()
+ * @method static Realms realms()
+ * @method static Groups groups()
+ * @method static Clients clients()
+ * @method static Organizations organizations()
+ * @method static AttackDetection attackDetection()
+ * @method static string getVersion()
+ * @method static ServerInfo serverInfo()
  */
 class KeycloakAdmin extends Facade
 {
